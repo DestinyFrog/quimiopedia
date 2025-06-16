b77cb93a84f0823fa9b13083ea71bb776a26c505
DestinyFrog <pedrocalizto9@gmail.com>
2025-06-15 23:23:56 -0300

    animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,31 @@
 
 const data_links_elements = document.querySelectorAll('.link-element')
 
-for ( const data_link_element of data_links_elements ) {
+for (const data_link_element of data_links_elements) {
     const value = data_link_element.getAttribute('value')
     const element = elements[value]
-    
+
     if (!element) continue
-    
+
+    const id = "#paper-" + value
+
     const a_link_element = document.createElement('a')
     a_link_element.innerHTML = element.symbol
-    a_link_element.href = "#paper-" + value
+    a_link_element.href = id
     a_link_element.classList.add('small-element')
     a_link_element.classList.add(element.category)
+
+    a_link_element.addEventListener('click', function () { openTab(id) })
 
     data_link_element.appendChild(a_link_element)
 }
 
 const data_elements = document.querySelectorAll('.element')
 
-for ( const data_element of data_elements ) {
+for (const data_element of data_elements) {
     const value = data_element.getAttribute('value')
     const element = elements[value]
-    
+
     data_element.classList.add('element')
     data_element.classList.add(element.category)
 
@@ -48,41 +52,61 @@ for ( const data_element of data_elements ) {
 
 const folders = document.querySelectorAll('.folder')
 
-for ( const folder of folders ) {
+for (const folder of folders) {
     const papers = folder.querySelectorAll('.paper')
 
-    const folder_front = document.createElement('div')
-    folder.appendChild(folder_front)
-
     let i = 0
-    for ( const paper of papers ) {
+    for (const paper of papers) {
         const el_title = paper.querySelector('.title')
         const title = el_title.innerHTML
 
         const div_menu = document.createElement('div')
         div_menu.classList.add('folder-tag')
-        
         const button_menu = document.createElement('button')
-        button_menu.style.marginLeft = (i*20)+'px'
+        button_menu.style.marginLeft = `mod(${i * 50}px, 70%)`
+        console.log(button_menu.style.marginLeft)
         button_menu.innerHTML = title
         div_menu.appendChild(button_menu)
-        
+
         const my_paper = paper
         paper.remove()
-        folder.appendChild(div_menu)
-        folder.appendChild(my_paper)
 
-        button_menu.addEventListener('click',
-            function () {
-                my_paper.classList.toggle('selected')
-                div_menu.classList.toggle('tag-selected')
-            })
+        const div_tab = document.createElement('div')
+        div_tab.appendChild(div_menu)
+        div_tab.appendChild(my_paper)
+        folder.appendChild(div_tab)
 
+        const paper_id = my_paper.getAttribute('id')
+        my_paper.removeAttribute('id')
+        div_tab.setAttribute('id', paper_id)
+
+        button_menu.addEventListener('click', function () { openTab("#" + paper_id) })
         i++
     }
+
+    const data_folder_title = folder.querySelector('.folder-data')
+    const folder_title = data_folder_title.getAttribute('value')
+
+    const folder_front = document.createElement('div')
+    folder_front.className = 'folder-front'
+    folder_front.innerHTML = folder_title
+    folder.appendChild(folder_front)
 }
 
-function selectPaper() {
-    my_paper.classList.toggle('selected')
-    div_menu.classList.toggle('tag-selected')
+function openTab(id) {
+    const paper = document.querySelector(id)
+
+    paper.classList.remove('start-paper')
+
+    if (paper.classList.contains('selected')) {
+        paper.classList.add('unselected')
+        paper.classList.remove('selected')
+    }
+    else {
+        paper.classList.add('selected')
+        paper.classList.remove('unselected')
+    }
+
 }
+
+openTab("#paper-dioxido-de-carbono")
