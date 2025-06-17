68bd84651ff0aed8a2df2708b9f2c23035d86768
DestinyFrog <pedrocalizto9@gmail.com>
2025-06-17 17:04:08 -0300

    form working

--- scripts/script.js
+++ scripts/script.js
@@ -64,7 +64,6 @@ for (const folder of folders) {
         div_menu.classList.add('folder-tag')
         const button_menu = document.createElement('button')
         button_menu.style.marginLeft = `mod(${i * 50}px, 70%)`
-        console.log(button_menu.style.marginLeft)
         button_menu.innerHTML = title
         div_menu.appendChild(button_menu)
 
@@ -110,3 +109,24 @@ function openTab(id) {
 }
 
 openTab("#paper-dioxido-de-carbono")
+
+const input_metano = document.querySelector('#input-metano')
+const input_gas_oxigenio = document.querySelector('#input-gas-oxigenio')
+const input_dioxido_de_carbono = document.querySelector('#input-dioxido-de-carbono')
+const input_agua = document.querySelector('#input-agua')
+
+input_metano.addEventListener('input', estequiometria_dioxido_de_carbono)
+
+function estequiometria_dioxido_de_carbono() {
+    const value_metano = input_metano.value
+
+    const gas_oxigenio = value_metano * 3
+    const dioxido_de_carbono = value_metano * 1
+    const agua = value_metano * 2
+
+    console.log(value_metano, gas_oxigenio, dioxido_de_carbono, agua)
+
+    input_gas_oxigenio.setAttribute('value', gas_oxigenio)
+    input_dioxido_de_carbono.setAttribute('value', dioxido_de_carbono)
+    input_agua.setAttribute('value', agua)
+}
